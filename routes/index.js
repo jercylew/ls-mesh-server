94354a0fcaa16421b52b385151896520cde84c6b
Yuming Liu <jercy_lew@yahoo.com>
2021-11-26 10:16:43 +0800

    Remove unused code in index

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-/* GET home page. */
-//router.get('/', function(req, res, next) {
-//  res.render('index', { title: 'Express' });
-//});
 router.get('/*', function(req, res, next) {
   console.log('Index rout ...');
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
