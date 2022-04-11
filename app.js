ca7e601f38f7bc7ee9e8ac8ad52c92c3c0bb5c14
Yuming Liu <jercy_lew@yahoo.com>
2022-04-11 17:31:59 +0800

    Add OPTIONS method for cors

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PATCH', 'DELETE'] }));
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'] }));
 app.use(bodyParser.json())
 
 app.use('/bebebus-app-front', bebebusAppFrontRouter);
