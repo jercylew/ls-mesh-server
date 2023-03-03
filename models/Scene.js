d8ff4281ffa9bf1187299c35714a52847d3ee1a9
Yuming Liu <jercy_lew@yahoo.com>
2023-03-03 11:31:56 +0800

    Add event log handling

--- models/Scene.js
+++ models/Scene.js
@@ -45,6 +45,7 @@ const SceneSchema = mongoose.Schema({
         // 漏电流(over_leak_current)，线路过流(ch_over_current)，线路过热(ch_over_heat)，
         // 网关断线(gateway_offline)，系统内存不足(sys_mem_full)，系统磁盘空间不足(sys_disk_full)，
         // 设备掉线(dev_offline), CO浓度过高(dev_co_over_threshold)
+        id: Number,
         type: String,
         level: Number, //0: Info(提示), 1: Warning（警告）, 2: Error（错误）
         message: String, // Detailed info, eg, "线路温度超过65，且已经持续60秒"
